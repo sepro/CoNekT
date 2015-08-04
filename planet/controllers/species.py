9dd40d2561a72acd852ea058a6fbae7509632c38
sepro <sebastian.proost@gmail.com>
2015-08-04 15:44:46 +0200

    Species overview added

--- planet/controllers/species.py
+++ planet/controllers/species.py
@@ -6,12 +6,12 @@ species = Blueprint('species', __name__)
 
 @species.route('/')
 def species_overview():
-    return "TEST OK"
+    all_species = Species.query.all()
+
+    return render_template('species.html', all_species=all_species)
 
 @species.route('/view/<species_id>')
 def species_view(species_id):
     current_species = Species.query.get_or_404(species_id)
-
-    print(current_species.sequences.count())
 
     return render_template('species.html', species=current_species)
