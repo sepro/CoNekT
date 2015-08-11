e189104e8bb5d0157ec8833ca2970430e9622ecd
sepro <sebastian.proost@gmail.com>
2015-08-11 14:58:42 +0200

    line plots for expression data working

--- planet/controllers/expression_profile.py
+++ planet/controllers/expression_profile.py
@@ -19,8 +19,9 @@ def expression_profile_view(profile_id):
 
     return render_template("expression_profile.html", profile=current_profile)
 
-@expression_profile.route('/json/<profile_id>')
-def expression_profile_json(profile_id):
+
+@expression_profile.route('/json/radar/<profile_id>')
+def expression_profile_radar_json(profile_id):
     current_profile = ExpressionProfile.query.get_or_404(profile_id)
     data = json.loads(current_profile.profile)
 
@@ -42,3 +43,49 @@ def expression_profile_json(profile_id):
 
     return Response(json.dumps(output), mimetype='application/json')
 
+
+@expression_profile.route('/json/plot/<profile_id>')
+def expression_profile_plot_json(profile_id):
+    current_profile = ExpressionProfile.query.get_or_404(profile_id)
+    data = json.loads(current_profile.profile)
+
+    processed_means = {}
+    processed_mins = {}
+    processed_maxs = {}
+    for key, expression_values in data["data"].items():
+        processed_means[key] = mean(expression_values)
+        processed_mins[key] = min(expression_values)
+        processed_maxs[key] = max(expression_values)
+
+
+    output = {"labels": list(data["order"]),
+              "datasets": [{
+                    "label": "Expression Profile for " + current_profile.probe,
+                    "fillColor": "rgba(220,220,220,0.2)",
+                    "strokeColor": "rgba(175,175,175,1)",
+                    "pointColor": "rgba(220,220,220,1)",
+                    "pointStrokeColor": "#fff",
+                    "pointHighlightFill": "#fff",
+                    "pointHighlightStroke": "rgba(220,220,220,1)",
+                    "data": list([processed_means[c] for c in data["order"]])},
+                  {
+                    "label": "Minimum",
+                    "fillColor": "rgba(220,220,220,0)",
+                    "strokeColor": "rgba(220,22,22,0)",
+                    "pointColor": "rgba(220,22,22,1)",
+                    "pointStrokeColor": "#fff",
+                    "pointHighlightFill": "#fff",
+                    "pointHighlightStroke": "rgba(220,220,220,1)",
+                    "data": list([processed_mins[c] for c in data["order"]])},
+                  {
+                    "label": "Maximum expression",
+                    "fillColor": "rgba(220,220,220,0)",
+                    "strokeColor": "rgba(220,22,22,0)",
+                    "pointColor": "rgba(220,22,22,1)",
+                    "pointStrokeColor": "#fff",
+                    "pointHighlightFill": "#fff",
+                    "pointHighlightStroke": "rgba(220,220,220,1)",
+                    "data": list([processed_maxs[c] for c in data["order"]])}
+              ]}
+
+    return Response(json.dumps(output), mimetype='application/json')
