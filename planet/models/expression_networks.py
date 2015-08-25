a9b03016e91de8c32060c5300afbe3b7d2e30f83
proost <proost@mpimp-golm.mpg.de>
2015-08-25 17:27:55 +0200

    Started to refactor code to get network (should be part of ExpressionNetwork model) and the ExpressionNetworkCytoscape should use this

--- planet/models/expression_networks.py
+++ planet/models/expression_networks.py
@@ -1,4 +1,8 @@
+from sqlalchemy import and_
+
 from planet import db
+
+import json
 
 
 class ExpressionNetworkMethod(db.Model):
@@ -29,3 +33,106 @@ class ExpressionNetwork(db.Model):
         self.gene_id = gene_id
         self.network = network
         self.method_id = method_id
+
+    @staticmethod
+    def get_neighborhood(probe, depth=0):
+        node = ExpressionNetwork.query.get(probe)
+        links = json.loads(node.network)
+
+        method_id = node.method_id
+        edge_type = node.method.edge_type
+
+        # add the initial node
+        nodes = [{"id": node.probe,
+                  "name": node.probe,
+                  "gene_id": node.gene_id,
+                  "gene_name": node.gene.name,
+                  "node_type": "query"}]
+        edges = []
+
+        # lists necessary for doing deeper searches
+        additional_nodes = []
+        existing_edges = []
+        existing_nodes = [node.probe]
+
+        # add direct neighbors of the gene of interest
+
+        for link in links:
+            nodes.append(ExpressionNetwork.__process_link(link))
+            edges.append({"source": node.probe,
+                          "target": link["probe_name"],
+                          "depth": 0,
+                          "link_score": link["link_score"],
+                          "edge_type": edge_type})
+            additional_nodes.append(link["probe_name"])
+            existing_edges.append([node.probe, link["probe_name"]])
+            existing_edges.append([link["probe_name"], node.probe])
+            existing_nodes.append(link["probe_name"])
+
+        # iterate n times to add deeper links
+
+        for i in range(1, depth+1):
+            new_nodes = ExpressionNetwork.\
+                query.filter(and_(ExpressionNetwork.probe.in_(additional_nodes),
+                                  ExpressionNetwork.method_id == method_id))
+            next_nodes = []
+
+            for new_node in new_nodes:
+                new_links = json.loads(new_node.network)
+
+                for link in new_links:
+                    if link["probe_name"] not in existing_nodes:
+                        nodes.append(ExpressionNetwork.__process_link(link))
+                        existing_nodes.append(link["probe_name"])
+                        next_nodes.append(link["probe_name"])
+
+                    if [new_node.probe, link["probe_name"]] not in existing_edges:
+                        edges.append({"source": new_node.probe,
+                                      "target": link["probe_name"],
+                                      "depth": i,
+                                      "link_score": link["link_score"],
+                                      "edge_type": edge_type})
+                        existing_edges.append([new_node.probe, link["probe_name"]])
+                        existing_edges.append([link["probe_name"], new_node.probe])
+
+            additional_nodes = next_nodes
+
+        # Add links between the last set of nodes added
+        new_nodes = ExpressionNetwork.query.filter(and_(ExpressionNetwork.probe.in_(additional_nodes),
+                                                        ExpressionNetwork.method_id == method_id))
+        for new_node in new_nodes:
+            new_links = json.loads(new_node.network)
+            for link in new_links:
+                if link["probe_name"] in existing_nodes:
+                    if [new_node.probe, link["probe_name"]] not in existing_edges:
+                        edges.append({"source": new_node.probe,
+                                      "target": link["probe_name"],
+                                      "depth": depth+1,
+                                      "link_score": link["link_score"],
+                                      "edge_type": edge_type})
+                        existing_edges.append([new_node.probe, link["probe_name"]])
+                        existing_edges.append([link["probe_name"], new_node.probe])
+
+        return {"nodes": nodes, "edges": edges}
+
+    @staticmethod
+    def __process_link(linked_probe):
+        """
+        Internal function that processes a linked probe (from the ExpressionNetwork.network field) to a data entry
+        compatible with cytoscape.js
+
+        :param linked_probe: hash with information from ExpressionNetwork.network field
+        :return: a hash formatted for use as a node with cytoscape.js
+        """
+        if linked_probe["gene_id"] is not None:
+            return {"id": linked_probe["probe_name"],
+                    "name": linked_probe["probe_name"],
+                    "gene_id": linked_probe["gene_id"],
+                    "gene_name": linked_probe["gene_name"],
+                    "node_type": "linked"}
+        else:
+            return {"id": linked_probe["probe_name"],
+                    "name": linked_probe["probe_name"],
+                    "gene_id": None,
+                    "gene_name": linked_probe["gene_name"],
+                    "node_type": "linked"}
