d5a1bf36111563c41270705d08b395e2e26a764b
proost <proost@mpimp-golm.mpg.de>
2015-09-02 17:22:48 +0200

    played around with charts.js (improved tooltips and now profiles can be compared)

--- planet/controllers/expression_profile.py
+++ planet/controllers/expression_profile.py
@@ -29,6 +29,18 @@ def expression_profile_view(profile_id):
     return render_template("expression_profile.html", profile=current_profile)
 
 
+@expression_profile.route('/compare/<first_profile_id>/<second_profile_id>')
+def expression_profile_compare(first_profile_id, second_profile_id):
+    """
+    Gets expression profile data from the database and renders it.
+
+    :param profile_id: ID of the profile to show
+    """
+
+    return render_template("compare_profiles.html", first_profile_id=first_profile_id,
+                           second_profile_id=second_profile_id)
+
+
 @expression_profile.route('/find/<probe>')
 def expression_profile_find(probe):
     """
@@ -82,15 +94,15 @@ def expression_profile_plot_json(profile_id):
     processed_means = {}
     processed_mins = {}
     processed_maxs = {}
+
     for key, expression_values in data["data"].items():
         processed_means[key] = mean(expression_values)
         processed_mins[key] = min(expression_values)
         processed_maxs[key] = max(expression_values)
 
-
     output = {"labels": list(data["order"]),
               "datasets": [{
-                    "label": "Expression Profile for " + current_profile.probe,
+                    "label": "Mean",
                     "fillColor": "rgba(220,220,220,0.2)",
                     "strokeColor": "rgba(175,175,175,1)",
                     "pointColor": "rgba(220,220,220,1)",
@@ -108,7 +120,7 @@ def expression_profile_plot_json(profile_id):
                     "pointHighlightStroke": "rgba(220,220,220,1)",
                     "data": list([processed_mins[c] for c in data["order"]])},
                   {
-                    "label": "Maximum expression",
+                    "label": "Maximum",
                     "fillColor": "rgba(220,220,220,0)",
                     "strokeColor": "rgba(220,22,22,0)",
                     "pointColor": "rgba(220,22,22,1)",
@@ -116,5 +128,48 @@ def expression_profile_plot_json(profile_id):
                     "pointHighlightFill": "#fff",
                     "pointHighlightStroke": "rgba(220,220,220,1)",
                     "data": list([processed_maxs[c] for c in data["order"]])}]}
+
+    return Response(json.dumps(output), mimetype='application/json')
+
+
+@expression_profile.route('/json/compare_plot/<first_profile_id>/<second_profile_id>')
+def expression_profile_compare_plot_json(first_profile_id, second_profile_id):
+    """
+    Generates a JSON object that can be rendered using Chart.js line plots
+
+    :param profile_id: ID of the profile to render
+    """
+    first_profile = ExpressionProfile.query.get_or_404(first_profile_id)
+    second_profile = ExpressionProfile.query.get_or_404(second_profile_id)
+    data_first = json.loads(first_profile.profile)
+    data_second = json.loads(second_profile.profile)
+
+    processed_first_means = {}
+    processed_second_means = {}
+
+    for key, expression_values in data_first["data"].items():
+        processed_first_means[key] = mean(expression_values)
+    for key, expression_values in data_second["data"].items():
+        processed_second_means[key] = mean(expression_values)
+
+    output = {"labels": list(data_first["order"]),
+              "datasets": [{
+                  "label": first_profile.probe,
+                  "fillColor": "rgba(220,110,110,0.2)",
+                  "strokeColor": "rgba(175,87,87,1)",
+                  "pointColor": "rgba(220,110,110,1)",
+                  "pointStrokeColor": "#fff",
+                  "pointHighlightFill": "#fff",
+                  "pointHighlightStroke": "rgba(220,220,220,1)",
+                  "data": list([processed_first_means[c] for c in data_first["order"]])},
+                  {
+                  "label": second_profile.probe,
+                  "fillColor": "rgba(110,110,220,0.2)",
+                  "strokeColor": "rgba(87,87,175,1)",
+                  "pointColor": "rgba(110,110,220,1)",
+                  "pointStrokeColor": "#fff",
+                  "pointHighlightFill": "#fff",
+                  "pointHighlightStroke": "rgba(220,220,220,1)",
+                  "data": list([processed_second_means[c] for c in data_second["order"]])}]}
 
     return Response(json.dumps(output), mimetype='application/json')
