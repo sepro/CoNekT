185bc064d6e1bbc4352c235df07219e248ccc604
proost <proost@mpimp-golm.mpg.de>
2015-11-10 18:10:49 +0100

    Cleaned code of XGMML export

--- planet/static/js/planet_xgmml.js
+++ planet/static/js/planet_xgmml.js
@@ -50,64 +50,64 @@ function writeXGMML(data) {
         xw.writeEndElement();
 
         data.elements.nodes.forEach( function(node) {
-          xw.writeStartElement( 'node' );
-          xw.writeAttributeString( 'id', node.data.id );
-          xw.writeAttributeString( 'label', node.data.name );
-          xw.writeAttributeString( 'name', 'base' );
+              xw.writeStartElement( 'node' );
+              xw.writeAttributeString( 'id', node.data.id );
+              xw.writeAttributeString( 'label', node.data.name );
+              xw.writeAttributeString( 'name', 'base' );
 
-              xw.writeStartElement( 'att' );
-              xw.writeAttributeString( 'label', 'gene_name' );
-              xw.writeAttributeString( 'name', 'gene_name' );
-              xw.writeAttributeString( 'value', node.data.gene_name );
-              xw.writeAttributeString( 'type', 'string');
+                    xw.writeStartElement( 'att' );
+                    xw.writeAttributeString( 'label', 'gene_name' );
+                    xw.writeAttributeString( 'name', 'gene_name' );
+                    xw.writeAttributeString( 'value', node.data.gene_name );
+                    xw.writeAttributeString( 'type', 'string');
+                    xw.writeEndElement();
+
+                    xw.writeStartElement( 'graphics' );
+                    xw.writeAttributeString( 'width', '1' );
+                    xw.writeAttributeString( 'fill', __convertColor(node.data.current_color) );
+                    xw.writeAttributeString( 'outline', "#000000" );
+                    xw.writeAttributeString( 'x', node.position.x);
+                    xw.writeAttributeString( 'y', node.position.y);
+                    xw.writeAttributeString( 'h', '30.0');
+                    xw.writeAttributeString( 'w', '30.0');
+                    xw.writeAttributeString( 'type', __convertShape(node.data.current_shape) );
+
+                        xw.writeStartElement( 'att' );
+                        xw.writeAttributeString( 'name', 'cytoscapeNodeGraphicsAttributes' );
+                            xw.writeStartElement( 'att' );
+                            xw.writeAttributeString( 'name', 'nodeTransparency' );
+                            xw.writeAttributeString( 'value', '1.0' );
+                            xw.writeEndElement();
+
+                            xw.writeStartElement( 'att' );
+                            xw.writeAttributeString( 'name', 'nodeLabelFont' );
+                            xw.writeAttributeString( 'value', 'Default-0-12' );
+                            xw.writeEndElement();
+
+                            xw.writeStartElement( 'att' );
+                            xw.writeAttributeString( 'name', 'borderLineType' );
+                            xw.writeAttributeString( 'value', 'solid' );
+                            xw.writeEndElement();
+                        xw.writeEndElement();
+
+                    xw.writeEndElement();
+
               xw.writeEndElement();
-
-                xw.writeStartElement( 'graphics' );
-                xw.writeAttributeString( 'width', '1' );
-                xw.writeAttributeString( 'fill', __convertColor(node.data.current_color) );
-                xw.writeAttributeString( 'outline', "#000000" );
-                xw.writeAttributeString( 'x', node.position.x);
-                xw.writeAttributeString( 'y', node.position.y);
-                xw.writeAttributeString( 'h', '30.0');
-                xw.writeAttributeString( 'w', '30.0');
-                xw.writeAttributeString( 'type', __convertShape(node.data.current_shape) );
-
-                  xw.writeStartElement( 'att' );
-                  xw.writeAttributeString( 'name', 'cytoscapeNodeGraphicsAttributes' );
-                      xw.writeStartElement( 'att' );
-                      xw.writeAttributeString( 'name', 'nodeTransparency' );
-                      xw.writeAttributeString( 'value', '1.0' );
-                      xw.writeEndElement();
-
-                      xw.writeStartElement( 'att' );
-                      xw.writeAttributeString( 'name', 'nodeLabelFont' );
-                      xw.writeAttributeString( 'value', 'Default-0-12' );
-                      xw.writeEndElement();
-
-                      xw.writeStartElement( 'att' );
-                      xw.writeAttributeString( 'name', 'borderLineType' );
-                      xw.writeAttributeString( 'value', 'solid' );
-                      xw.writeEndElement();
-                  xw.writeEndElement();
-
-                xw.writeEndElement();
-
-          xw.writeEndElement();
         });
 
         data.elements.edges.forEach( function(edge) {
-          xw.writeStartElement( 'edge' );
-          xw.writeAttributeString( 'id', edge.data.id );
-          xw.writeAttributeString( 'label', edge.data.id );
-          xw.writeAttributeString( 'source', edge.data.source );
-          xw.writeAttributeString( 'target', edge.data.target );
+            xw.writeStartElement( 'edge' );
+            xw.writeAttributeString( 'id', edge.data.id );
+            xw.writeAttributeString( 'label', edge.data.id );
+            xw.writeAttributeString( 'source', edge.data.source );
+            xw.writeAttributeString( 'target', edge.data.target );
 
-            xw.writeStartElement( 'graphics' );
-            xw.writeAttributeString( 'width', edge.data.current_width.replace('px', '') );
-            xw.writeAttributeString( 'fill', __convertColor(edge.data.current_color) );
+                xw.writeStartElement( 'graphics' );
+                xw.writeAttributeString( 'width', edge.data.current_width.replace('px', '') );
+                xw.writeAttributeString( 'fill', __convertColor(edge.data.current_color) );
+                xw.writeEndElement();
+
             xw.writeEndElement();
-
-          xw.writeEndElement();
 
         });
 
