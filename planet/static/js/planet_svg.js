233443b3cdfbea22f7dfd818665c535faf6a881a
proost <proost@mpimp-golm.mpg.de>
2015-11-19 11:08:17 +0100

    Continued SVG export using pablo.js (nodes are working !)

--- planet/static/js/planet_svg.js
+++ planet/static/js/planet_svg.js
@@ -27,9 +27,41 @@ function writeSVG(data) {
     data.elements.nodes.forEach( function(node) {
         var group = svg.g();
         group.transform('translate', node.position.x, node.position.y)
-        group.circle({r:15, fill:__convertColor(node.data.current_color)});
-        group.text({fill:"none", stroke:"black"}).content(node.data.name).attr('text-anchor', 'middle');
-        group.text({fill:"white"}).content(node.data.name).attr('text-anchor', 'middle');
+
+        if (node.data.current_shape === 'ellipse') {
+            group.circle({r:15, fill:__convertColor(node.data.current_color)});
+        } else if (node.data.current_shape === 'rectangle') {
+            group.rect({x:-15, y:-15, width:30, height:30, fill:__convertColor(node.data.current_color)});
+        } else if (node.data.current_shape === 'roundrectangle') {
+            group.rect({x:-15, y:-15, rx:5, ry:5, width:30, height:30, fill:__convertColor(node.data.current_color)});
+        } else if (node.data.current_shape === 'triangle') {
+            group.polygon({points:'0,-20 17,10 -17,10', fill:__convertColor(node.data.current_color)});
+        } else if (node.data.current_shape === 'hexagon') {
+            group.polygon({points:'0,15 13,7.5 13,-7.5 0,-15 -13,-7.5 -13,7.5', fill:__convertColor(node.data.current_color)})
+                .transform('rotate', 30);
+        } else if (node.data.current_shape === 'octagon') {
+            group.polygon({points:'0,15 10.6,10.6 15,0 10.6,-10.6 0,-15 -10.6,-10.6 -15,0 -10.6,10.6', fill:__convertColor(node.data.current_color)})
+                .transform('rotate', 22.5);
+        } else if (node.data.current_shape === 'vee') {
+            group.polygon({points:'0,20 17,-10 0,0 -17,-10', fill:__convertColor(node.data.current_color)});
+        } else if (node.data.current_shape === 'diamond') {
+            group.rect({x:-13, y:-13, width:26, height:26, fill:__convertColor(node.data.current_color)})
+            .transform('rotate', 45);
+        } else if (node.data.current_shape === 'rhomboid') {
+            group.rect({x:-13, y:-15, width:26, height:30, fill:__convertColor(node.data.current_color)})
+            .transform('skewX', 30);
+        }
+
+        text = node.data.name;
+        if (node.data.gene_name !== '') { text = node.data.gene_name; }
+
+        group.text({y:6, fill:"none", stroke:'#888888'}).content(text)
+            .attr('text-anchor', 'middle')
+            .attr('font-family', 'sans-serif');
+        group.text({y:6, fill:"white"}).content(text)
+            .attr('text-anchor', 'middle')
+            .attr('font-family', 'sans-serif');
+
     });
 
     data.elements.edges.forEach( function(edge) {
