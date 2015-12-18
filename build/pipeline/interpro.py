692997b8d984a82f2f25afdb0be0893d303c0605
proost <proost@mpimp-golm.mpg.de>
2015-12-18 15:03:37 +0100

    further work on interpro pipeline

--- build/pipeline/interpro.py
+++ build/pipeline/interpro.py
@@ -1,11 +1,13 @@
 import os
+import configparser
 
 from utils.parser.fasta import Fasta
 from math import ceil
 
 
-def generate_script(filename, job_name, job_count, input_string, output_string, interpro_module=None, interpro_cmd="interproscan.sh"):
+def generate_script(filename, job_name, job_count, input_string, output_string, interpro_module=None, interpro_cmd="interproscan.sh", email=None):
     load_module = "" if interpro_module is None else "module load " + interpro_module
+    include_email = "" if email is None else "#$ -m bea\n#$ -M " + email
 
     input_string = input_string.replace('%d', "${SGE_TASK_ID}")
     output_string = output_string.replace('%d', "${SGE_TASK_ID}")
@@ -23,9 +25,8 @@ def generate_script(filename, job_name, job_count, input_string, output_string, 
 #$ -o OUT_$JOB_NAME.$JOB_ID
 #$ -e ERR_$JOB_NAME.$JOB_ID
 
-
-#$ -m bea
-#$ -M proost@mpimp-golm.mpg.de
+#email
+%s
 
 #
 %s
@@ -33,7 +34,7 @@ date
 hostname
 %s -i %s -o %s -f tsv -dp -iprlookup -goterms
 date
-""" % (job_name, job_count, load_module, interpro_cmd, input_string, output_string)
+""" % (job_name, job_count, include_email, load_module, interpro_cmd, input_string, output_string)
 
     with open(filename, 'w') as f:
         print(output, file=f)
@@ -58,5 +59,31 @@ def split_fasta(file, chunks, output_directory, filenames="proteins_%d.fasta"):
 
 def run_interpro(config):
     print("Running InterProScan using", config)
+
+    cp = configparser.ConfigParser()
+    cp.read(config)
+
+    interpro_module = cp['GENERAL']['interpro_module']
+    interpro_cmd = cp['GENERAL']['interpro_cmd']
+    genomes = cp['GENERAL']['genomes'].split(';')
+    email = cp['GENERAL']['email']
+    jobs = cp['GENERAL']['jobs']
+
+    for g in genomes:
+        input = cp[g]['input']
+        output = cp[g]['output']
+        split_filenames = cp[g]['split_filenames']
+        out_filenames = cp[g]['out_filenames']
+
+        script = 'run_interpro_'+g+'.sh'
+        job_name = 'interproscan_' + g
+
+        split_fasta(input, jobs, output, filenames=split_filenames)
+        generate_script(script, job_name, jobs,
+                        os.path.join(output, split_filenames),
+                        os.path.join(output, out_filenames),
+                        interpro_module=interpro_module,
+                        interpro_cmd=interpro_cmd,
+                        email=email)
 
     print("Done !")
