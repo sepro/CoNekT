00ffd1d586826ed918e2bbe64869c5b4c4520036
proost <proost@mpimp-golm.mpg.de>
2015-12-18 13:54:41 +0100

    further work on interpro pipeline

--- build/pipeline/interpro.py
+++ build/pipeline/interpro.py
@@ -1,12 +1,30 @@
-from utils.parser import fasta
+import os
+
+from utils.parser.fasta import Fasta
+from math import ceil
 
 
 def generate_script(filename):
 
     return filename
 
+
 def split_fasta(file, chunks, output_directory):
-    pass
+    fasta = Fasta()
+    fasta.readfile(file)
+
+    seq_per_chunk = ceil(len(fasta.sequences.keys())/chunks)
+
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    for i in range(1, chunks):
+        subset = fasta.remove_subset(seq_per_chunk)
+        filename = "proteins_%d.fasta" % i
+        filename = os.path.join(output_directory, filename)
+
+        subset.writefile(filename)
+
 
 def run_interpro(config):
     print("Running InterProScan using", config)
--- utils/parser/fasta.py
+++ utils/parser/fasta.py
@@ -5,7 +5,27 @@ class Fasta:
     def __init__(self):
         self.sequences = {}
 
+    def remove_subset(self, length):
+        """
+        Removes a set of sequences and returns those as a subset
+
+        :param length: number of sequences to remove
+        :return: Fasta object with the sequences removed from the current one
+        """
+        output = Fasta()
+        keys = list(self.sequences.keys())
+        output.sequences = {k: self.sequences[k] for k in keys[:length]}
+
+        self.sequences = {k: self.sequences[k] for k in keys[length:]}
+
+        return output
+
     def readfile(self, filename):
+        """
+        Reads a fasta file to the dictionary
+
+        :param filename: file to read
+        """
         print("Reading FASTA file:" + filename + "...", file=sys.stderr)
 
         # Initialize variables
@@ -33,3 +53,14 @@ class Fasta:
 
         f.close()
         print("Done! (found ", count, " sequences)", file=sys.stderr)
+
+    def writefile(self, filename):
+        """
+        writes the sequences back to a fasta file
+
+        :param filename: file to write to
+        """
+        with open(filename, 'w') as f:
+            for k, v in self.sequences.items():
+                print(">" + k, file=f)
+                print(v, file=f)
