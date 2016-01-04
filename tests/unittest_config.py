7e0d131cf39154a8d8abc6b69bab6c7b730334b2
proost <proost@mpimp-golm.mpg.de>
2016-01-04 09:50:03 +0100

    implementing proper structure for unit testing

---
+++ tests/unittest_config.py
@@ -0,0 +1,61 @@
+"""
+Configuration of the website and database.
+
+Copy this file to config.py and change the settings accordingly
+"""
+import os
+import tempfile
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Flask settings, make sure to set the SECRET_KEY and turn DEBUG and TESTING to False for production
+DEBUG = False
+TESTING = True
+
+SECRET_KEY = 'DragonsLiveHere'
+
+# Login settings + admin account
+LOGIN_ENABLED = True
+ADMIN_PASSWORD = 'admin'
+ADMIN_EMAIL = 'admin@web.com'
+
+
+# Database settings, database location and path to migration scripts
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db', 'fungi.db')
+DATABASE_PATH = tempfile.mkstemp()
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'migration')
+SQLALCHEMY_ECHO = DEBUG
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# Collation type, NOCASE for sqlite, '' for MySQL
+SQL_COLLATION = 'NOCASE'
+
+# Settings for the FTP/bulk data
+PLANET_FTP_DATA = os.path.join(basedir, 'ftp')
+
+# Settings for Cache
+CACHE_TYPE = 'null'
+CACHE_DEFAULT_TIMEOUT = 600
+CACHE_THRESHOLD = 10000
+CACHE_NO_NULL_WARNING = True
+
+# Minify pages when debug is off
+MINIFY_PAGE = not DEBUG
+
+# BLAST settings
+BLAST_ENABLED = False
+# BLAST_TMP_DIR = tempfile.mkdtemp()
+BLAST_TMP_DIR = 'C:/Users/Seb/AppData/Local/Temp/'
+
+BLASTP_PATH = 'D:/blast/bin/blastp.exe'
+BLASTP_DB_PATH = 'D:/blast/db/ath_db'
+BLASTN_PATH = 'D:/blast/bin/blastn.exe'
+BLASTN_DB_PATH = 'D:/blast/db/ath_cds_db'
+
+BLASTP_CMD = BLASTP_PATH + ' -db ' + BLASTP_DB_PATH + ' -query <IN> -out <OUT> -outfmt 6 -num_threads 1'
+BLASTN_CMD = BLASTN_PATH + ' -db ' + BLASTN_DB_PATH + ' -query <IN> -out <OUT> -outfmt 6 -num_threads 1'
+
+# Debug settings
+DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+
---
+++ tests/website.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+from planet import create_app
+
+from planet.models.sequences import Sequence
+from planet.models.species import Species
+from planet.models.interpro import Interpro
+from planet.models.go import GO
+from planet.models.gene_families import GeneFamily
+
+from flask.ext.testing import TestCase
+
+import sys
+
+
+class WebsiteTest(TestCase):
+
+    def create_app(self):
+        app = create_app('config')
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_ECHO'] = False
+        return app
+
+    def test_sequence(self):
+        sequence = Sequence.query.first()
+        if sequence is not None:
+            response = self.client.get("/sequence/view/%d" % sequence.id)
+            self.assert_template_used('sequence.html')
+            self.assert200(response)
+
+            response = self.client.get("/sequence/fasta/coding/%d" % sequence.id)
+            self.assert200(response)
+
+            data = response.data.decode("utf-8").strip()
+            self.assertEqual(len(data.split('\n')), 2)
+            self.assertEqual(data[0], '>')
+
+            response = self.client.get("/sequence/fasta/protein/%d" % sequence.id)
+            self.assert200(response)
+
+            data = response.data.decode("utf-8").strip()
+            self.assertEqual(len(data.split('\n')), 2)
+            self.assertEqual(data[0], '>')
+
+            response = self.client.get("/sequence/find/" + sequence.name)
+            self.assert_template_used('sequence.html')
+            self.assert200(response)
+
+            response = self.client.get("/sequence/view/a")
+            self.assert404(response)
+        else:
+            print('  * test_sequence: No sequence found, skipping test...', file=sys.stderr)
+
+    def test_species(self):
+        response = self.client.get("/species/")
+        self.assert_template_used('species.html')
+        self.assert200(response)
+
+        species = Species.query.first()
+        if species is not None:
+            response = self.client.get("/species/view/%d" % species.id)
+            self.assert_template_used('species.html')
+            self.assert200(response)
+
+            response = self.client.get("/species/sequences/%d/1" % species.id)
+            self.assert_template_used('pagination/sequences.html')
+            self.assert200(response)
+
+            response = self.client.get("/species/download/coding/%d" % species.id)
+            self.assert200(response)
+
+            data = response.data.decode("utf-8").strip()
+            self.assertTrue(len(data.split('\n')) > 0)
+            self.assertEqual(data[0], '>')
+
+            response = self.client.get("/species/download/protein/%d" % species.id)
+            self.assert200(response)
+
+            data = response.data.decode("utf-8").strip()
+            self.assertTrue(len(data.split('\n')) > 0)
+            self.assertEqual(data[0], '>')
+
+            response = self.client.get("/species/stream/coding/%d" % species.id)
+            self.assert200(response)
+
+            data = response.data.decode("utf-8").strip()
+            self.assertTrue(len(data.split('\n')) > 0)
+            self.assertEqual(data[0], '>')
+
+            response = self.client.get("/species/stream/protein/%d" % species.id)
+            self.assert200(response)
+
+            data = response.data.decode("utf-8").strip()
+            self.assertTrue(len(data.split('\n')) > 0)
+            self.assertEqual(data[0], '>')
+
+        else:
+            print('  * test_species: No species found, skipping test...', file=sys.stderr)
+
+    def test_interpro(self):
+        interpro = Interpro.query.first()
+        if interpro is not None:
+            response = self.client.get("/interpro/view/%d" % interpro.id)
+            self.assert_template_used('interpro.html')
+            self.assert200(response)
+
+            response = self.client.get("/interpro/find/" + interpro.label)
+            self.assert_template_used('interpro.html')
+            self.assert200(response)
+
+            response = self.client.get("/interpro/sequences/%d/1" % interpro.id)
+            self.assert_template_used('pagination/sequences.html')
+            self.assert200(response)
+
+            response = self.client.get("/interpro/sequences/table/%d" % interpro.id)
+            self.assert_template_used('tables/sequences.csv')
+            self.assert200(response)
+
+            response = self.client.get("/interpro/json/species/%d" % interpro.id)
+            self.assert200(response)
+        else:
+            print('  * test_interpro: No interpro domain found, skipping test...', file=sys.stderr)
+
+    def test_go(self):
+        go = GO.query.first()
+
+        if go is not None:
+            response = self.client.get("/go/view/%d" % go.id)
+            self.assert_template_used('go.html')
+            self.assert200(response)
+
+            response = self.client.get("/go/find/" + go.label)
+            self.assertRedirects(response, "/go/view/%d" % go.id)
+
+            response = self.client.get("/go/sequences/%d/1" % go.id)
+            self.assert_template_used('pagination/sequences.html')
+            self.assert200(response)
+
+            response = self.client.get("/go/sequences/table/%d" % go.id)
+            self.assert_template_used('tables/sequences.csv')
+            self.assert200(response)
+
+            response = self.client.get("/go/json/species/%d" % go.id)
+            self.assert200(response)
+
+            response = self.client.get("/go/json/genes/" + go.label)
+            self.assert200(response)
+        else:
+            print('  * test_go: No go label found, skipping test...', file=sys.stderr)
+
+    def test_family(self):
+        family = GeneFamily.query.first()
+
+        if family is not None:
+            response = self.client.get("/family/view/%d" % family.id)
+            self.assert_template_used('family.html')
+            self.assert200(response)
+
+            response = self.client.get("/family/find/" + family.name)
+            self.assert_template_used('family.html')
+            self.assert200(response)
+
+            response = self.client.get("/family/sequences/%d/1" % family.id)
+            self.assert_template_used('pagination/sequences.html')
+            self.assert200(response)
+
+            response = self.client.get("/family/sequences/table/%d" % family.id)
+            self.assert_template_used('tables/sequences.csv')
+            self.assert200(response)
+
+            response = self.client.get("/family/json/species/%d" % family.id)
+            self.assert200(response)
+        else:
+            print('  * test_family: No family found, skipping test...', file=sys.stderr)
+
