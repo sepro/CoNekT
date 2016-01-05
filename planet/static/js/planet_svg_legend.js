4709012cb4741f7ac38c8c42f27727d9d54f94f8
proost <proost@mpimp-golm.mpg.de>
2016-01-05 17:38:06 +0100

    refactoring some code (js) to make way for the legend functions

--- planet/static/js/planet_svg_legend.js
+++ planet/static/js/planet_svg_legend.js
@@ -3,6 +3,11 @@ var svg_legend;
 function svg_legend_init(svg_file, container_id) {
      svg_legend = Pablo.load(svg_file, function(){
         this.appendTo(container_id);
-        svg_legend.find('g#score_group').attr('style', 'opacity:0');
+        svg_legend.find('[legend_type="edge_color"]').attr('style', 'opacity:0');
     });
 }
+
+$('.cy-edge-color').click(function() {
+    svg_legend.find('[legend_type="edge_color"]').attr('style', 'opacity:0');
+    svg_legend.find('[edge_color="' + $( this ).attr( 'attr' ) + '"]').attr('style', 'opacity:100');
+})
