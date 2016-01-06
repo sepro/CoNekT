e84e4354aaef5abf6de255e7f8fdb30aff56ac55
proost <proost@mpimp-golm.mpg.de>
2016-01-06 10:34:45 +0100

    Legend work for edges

--- planet/static/js/planet_svg_legend.js
+++ planet/static/js/planet_svg_legend.js
@@ -5,6 +5,9 @@ function svg_legend_init(svg_file, container_id) {
         this.appendTo(container_id);
         svg_legend.find('[edge_color]').attr('style', 'opacity:0');
         svg_legend.find('[edge_color="color"]').attr('style', 'opacity:100')
+
+        svg_legend.find('[edge_width]').attr('style', 'opacity:0');
+        svg_legend.find('[edge_width="default"]').attr('style', 'opacity:100')
     });
 }
 
@@ -12,4 +15,10 @@ $('.cy-edge-color').click(function(ev) {
     ev.preventDefault();
     svg_legend.find('[edge_color]').attr('style', 'opacity:0');
     svg_legend.find('[edge_color="' + $( this ).attr( 'attr' ) + '"]').attr('style', 'opacity:100');
+})
+
+$('.cy-edge-width').click(function(ev) {
+    ev.preventDefault();
+    svg_legend.find('[edge_width]').attr('style', 'opacity:0');
+    svg_legend.find('[edge_width="' + $( this ).attr( 'attr' ) + '"]').attr('style', 'opacity:100');
 })
