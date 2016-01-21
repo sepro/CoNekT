397eee746f0380ceeaae5fbe9aae9802ae424cf7
sepro <sebastian.proost@gmail.com>
2016-01-21 11:38:43 +0100

    now multiple pagination tables per page are possible

--- planet/static/js/planet_pagination.js
+++ planet/static/js/planet_pagination.js
@@ -1,63 +1,67 @@
 $( document ).ready(function() {
-    $('#planet-pagination').attr( "current_page", 1 );
 
-    function update(){
-        var base_url = $('#planet-pagination').attr( "base-url" );
-        var page_count = parseInt($('#planet-pagination').attr( "page-count" ));
-        var page = parseInt($('#planet-pagination').attr( "current_page"));
+    function update( panel ){
+        var base_url = panel.attr( "base-url" );
+        var page_count = parseInt(panel.attr( "page-count" ));
+        var page = parseInt(panel.attr( "current_page"));
 
-        $( "#planet-pagination" ).html('<br /><br /><div id="loading" class="pagination"><i class="fa fa-refresh fa-spin"></i></div>');
-        $( "#planet-pagination" ).load( base_url + page );
+        panel.html('<br /><br /><div id="loading" class="pagination"><i class="fa fa-refresh fa-spin"></i></div>');
+        panel.load( base_url + page );
 
         if (page == 1)
         {
-            $( "#planet-pagination-first" ).attr("disabled", true);
-            $( "#planet-pagination-back" ).attr("disabled", true);
+            panel.parent().find( ".planet-pagination-first" ).attr("disabled", true);
+            panel.parent().find( ".planet-pagination-back" ).attr("disabled", true);
         } else {
-            $( "#planet-pagination-first" ).attr("disabled", false);
-            $( "#planet-pagination-back" ).attr("disabled", false);
+            panel.parent().find( ".planet-pagination-first" ).attr("disabled", false);
+            panel.parent().find( ".planet-pagination-back" ).attr("disabled", false);
         }
 
         if (page == page_count)
         {
-            $( "#planet-pagination-last" ).attr("disabled", true);
-            $( "#planet-pagination-next" ).attr("disabled", true);
+            panel.parent().find( ".planet-pagination-last" ).attr("disabled", true);
+            panel.parent().find( ".planet-pagination-next" ).attr("disabled", true);
         } else {
-            $( "#planet-pagination-last" ).attr("disabled", false);
-            $( "#planet-pagination-next" ).attr("disabled", false);
+            panel.parent().find( ".planet-pagination-last" ).attr("disabled", false);
+            panel.parent().find( ".planet-pagination-next" ).attr("disabled", false);
         }
-
     }
 
-    update();
-
-    $( "#planet-pagination-first" ).click( function () {
-        $('#planet-pagination').attr( "current_page", 1);
-        update();
+    $('.planet-pagination').each(function() {
+        update($( this ));
     });
 
-    $( "#planet-pagination-last" ).click( function () {
-        var page_count = parseInt($('#planet-pagination').attr( "page-count" ));
-        $('#planet-pagination').attr( "current_page", page_count);
-        update();
+    $( ".planet-pagination-first" ).click( function () {
+        var panel = $( this ).closest('.planet-pagination-container').find('.planet-pagination');
+        panel.attr( "current_page", 1);
+        update(panel);
     });
 
-    $( "#planet-pagination-next" ).click( function () {
-        var page_count = parseInt($('#planet-pagination').attr( "page-count" ));
-        var current_page = parseInt($('#planet-pagination').attr( "current_page"));
+    $( ".planet-pagination-last" ).click( function () {
+        var panel = $( this ).closest('.planet-pagination-container').find('.planet-pagination');
+        var page_count = parseInt(panel.attr( "page-count" ));
+        panel.attr( "current_page", page_count);
+        update(panel);
+    });
+
+    $( ".planet-pagination-next" ).click( function () {
+        var panel = $( this ).closest('.planet-pagination-container').find('.planet-pagination');
+        var page_count = parseInt(panel.attr( "page-count" ));
+        var current_page = parseInt(panel.attr( "current_page"));
         if (current_page < page_count)
         {
-            $('#planet-pagination').attr( "current_page", current_page + 1);
+            panel.attr( "current_page", current_page + 1);
         }
-        update();
+        update(panel);
     });
 
-    $( "#planet-pagination-back" ).click( function () {
-        var current_page = parseInt($('#planet-pagination').attr( "current_page"));
+    $( ".planet-pagination-back" ).click( function () {
+        var panel = $( this ).closest('.planet-pagination-container').find('.planet-pagination');
+        var current_page = parseInt(panel.attr( "current_page"));
         if (current_page > 1)
         {
-            $('#planet-pagination').attr( "current_page", current_page - 1);
+            panel.attr( "current_page", current_page - 1);
         }
-        update();
+        update(panel);
     });
 });
