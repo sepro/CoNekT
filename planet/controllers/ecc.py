5f17076a6b223ddec49b85da14e9247ce88792f3
sepro <sebastian.proost@gmail.com>
2016-02-11 11:30:55 +0100

    further work on ecc

--- planet/controllers/ecc.py
+++ planet/controllers/ecc.py
@@ -4,6 +4,8 @@ from sqlalchemy import and_
 from planet.models.sequences import Sequence
 from planet.models.relationships import SequenceSequenceECCAssociation
 
+from planet.helpers.cytoscape import CytoscapeHelper
+
 import json
 
 ecc = Blueprint('ecc', __name__)
@@ -19,18 +21,39 @@ def ecc_overview():
 
 @ecc.route('/json/<int:sequence>/<int:network>/<int:family>')
 def ecc_graph_json(sequence, network, family):
-
+    # TODO: refactor ! getting the network should be property of relation or sequence!
     sequence = Sequence.query.get_or_404(sequence)
     data = sequence.ecc_query_associations.filter(and_(
             SequenceSequenceECCAssociation.query_network_method_id == network,
             SequenceSequenceECCAssociation.gene_family_method_id == family)).all()
 
-    output = []
+    # add the initial node
+    nodes = [{"id": sequence.id,
+              "name": sequence.name,
+              "species_id": sequence.species_id,
+              "gene_id": sequence.id,
+              "gene_name": sequence.name,
+              "network_method_id": network,
+              "node_type": "query"}]
+    edges = []
 
     for d in data:
-        output.append({
-            'query': d.query_id,
-            'target': d.target_id
-        })
+        nodes.append({"id": d.target_id,
+                      "name": d.target_sequence.name,
+                      "species_id": d.target_sequence.species_id,
+                      "gene_id": d.target_id,
+                      "network_method_id": d.target_network_method_id,
+                      "gene_name": d.target_sequence.name})
 
-    return json.dumps(output)
+        # TODO: add p-value and corrected p once implemented
+        edges.append({"source": sequence.name,
+                      "target": d.target_sequence.name,
+                      "ecc_score": d.ecc})
+
+    # TODO add next level of connectivity
+
+    network = {"nodes": nodes, "edges": edges}
+
+    cytoscape_network = CytoscapeHelper.parse_network(network)
+
+    return json.dumps(cytoscape_network)
