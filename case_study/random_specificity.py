e7030db6c8b116b9561099605d89d2af9fb42d9d
sepro <sebastian.proost@gmail.com>
2016-04-12 11:31:19 +0200

    improved random specificty

--- case_study/random_specificity.py
+++ case_study/random_specificity.py
@@ -9,32 +9,38 @@ from utils.expression import max_spm
 import json
 from statistics import mean, stdev
 from random import normalvariate
+from sys import argv
 
-app = create_app('config')
 
-with app.app_context():
-    profiles = ExpressionProfile.query.filter(ExpressionProfile.species_id == 1).order_by(func.random()).options(undefer('profile')).limit(10000)
+def write_random_spms(species_id, filename):
+    app = create_app('config')
 
-    with open('./data/random_max_spm.txt', "w") as f:
-        for p in profiles:
-            if p.profile is not None:
-                profile_data = json.loads(p.profile)
+    with app.app_context():
+        profiles = ExpressionProfile.query.filter(ExpressionProfile.species_id == species_id).order_by(func.random()).options(undefer('profile')).limit(10000)
 
-                values = [float(sum(v)) for k, v in profile_data['data'].items()]
-                mean_value = mean(values)
-                sd = stdev(values)
+        with open(filename, "w") as f:
+            for p in profiles:
+                if p.profile is not None:
+                    profile_data = json.loads(p.profile)
 
-                for _ in range(10):
-                    random_values = []
+                    values = [float(sum(v)) for k, v in profile_data['data'].items()]
+                    mean_value = mean(values)
+                    sd = stdev(values)
 
-                    while len(random_values) < len(values):
-                        r = normalvariate(mean_value, sd)
-                        if r >= 0:
-                            random_values.append(r)
+                    for _ in range(10):
+                        random_values = []
 
-                    rand_mean = mean(random_values)
-                    rand_sd = stdev(random_values)
+                        while len(random_values) < len(values):
+                            r = normalvariate(mean_value, sd)
+                            if r >= 0:
+                                random_values.append(r)
 
-                    rand_max_spm = max_spm({c: v for c, v in enumerate(random_values)})['score']
+                        rand_mean = mean(random_values)
+                        rand_sd = stdev(random_values)
 
-                    print(rand_max_spm, file=f)
+                        rand_max_spm = max_spm({c: v for c, v in enumerate(random_values)})['score']
+
+                        print(rand_max_spm, file=f)
+
+if __name__ == "__main__":
+    write_random_spms(int(argv[1]), argv[2])
