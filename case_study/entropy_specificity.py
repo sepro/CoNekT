6d2bf6bfb384c0ac9bad9c3913cda64a29bba931
sepro <sebastian.proost@gmail.com>
2016-04-12 09:10:54 +0200

    better export of entropy/specificty data

--- case_study/entropy_specificity.py
+++ case_study/entropy_specificity.py
@@ -1,6 +1,6 @@
 from planet import create_app
 from planet.models.expression_profiles import ExpressionProfile
-from planet.models.expression_specificity import ExpressionSpecificity
+from planet.models.expression_specificity import ExpressionSpecificity, ExpressionSpecificityMethod
 
 from sqlalchemy.orm import joinedload
 
@@ -8,12 +8,15 @@ app = create_app('config')
 
 with app.app_context():
     profiles = ExpressionProfile.query.all()
+    methods = ExpressionSpecificityMethod.query.all()
 
-    with open("./data/entropy_specificity.txt", "w") as f_out:
-        for p in profiles:
-            s = p.specificities.filter(ExpressionSpecificity.method.has(description='Condition specificity')).order_by(ExpressionSpecificity.score.desc()).first()
-            if s is not None:
-                spm = s.score
-                method = s.method.description
-                condition = s.condition
-                print(p.id, p.probe, p.sequence.name if p.sequence is not None else None, p.species.name, p.entropy, spm, condition, method, sep='\t', file=f_out)
+    with open("./data/entropy_specificity_full.txt", "w") as f_out:
+        for m in methods:
+            for p in profiles:
+                if m.species_id == p.species_id:
+                    s = p.specificities.filter(ExpressionSpecificity.method_id == m.id).order_by(ExpressionSpecificity.score.desc()).first()
+                    if s is not None:
+                        spm = s.score
+                        method = s.method.description
+                        condition = s.condition
+                        print(p.id, p.probe, p.sequence.name if p.sequence is not None else None, p.species.name, p.entropy, spm, condition, method, sep='\t', file=f_out)
