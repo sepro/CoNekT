e336efb0158b291dd93750d4570c0dcbac565e1c
sepro <sebastian.proost@gmail.com>
2016-04-13 11:35:13 +0200

    removed radar plots form profile page (to be replaced with tissue condition)

--- build/pipeline/transcriptome.py
+++ build/pipeline/transcriptome.py
@@ -7,18 +7,29 @@ from utils.cluster.templates import bowtie_build_template
 
 
 class TranscriptomePipeline:
-
+    """
+    TranscriptomePipeline class. Reads a settings ini file and runs the transcriptome pipeline
+    """
     def __init__(self, config):
+        """
+        Constructor run with path to ini file with settings
+
+        :param config: path to setttings ini file
+        """
         self.cp = configparser.ConfigParser()
         self.cp.read(config)
 
     def prepare_genome(self):
+        """
+        Runs bowtie-build for each genome on the cluster. All settings are obtained from the settings fasta file
+        """
         bowtie_module = self.cp['DEFAULT']['bowtie_module']
         bowtie_build_cmd = self.cp['DEFAULT']['bowtie_build_cmd']
 
         genomes = self.cp['DEFAULT']['genomes'].split(';')
         email = None if self.cp['DEFAULT']['email'] == 'None' else self.cp['DEFAULT']['email']
 
+        # Filename should include a unique timestamp !
         filename = "bowtie_build_%d.sh" % int(time.time())
 
         template = bowtie_build_template("bowtie build", email, bowtie_module, bowtie_build_cmd)
