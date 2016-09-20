09b4fe72f164012ea79bb9d45de4f781b22db09a
sepro <sebastian.proost@gmail.com>
2016-09-20 14:32:00 +0200

    added code coverage

--- run_unittest.py
+++ run_unittest.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 import unittest
+import os
 
 from tests.website import WebsiteTest
 from tests.utils import UtilsTest
 
+from coverage import coverage
+
+cov = coverage(branch=True, omit=['virtualenv/*', 'tests.py'])
+cov.start()
+
 if __name__ == '__main__':
-    unittest.main(failfast=True)
+    try:
+        unittest.main(failfast=True)
+    except:
+        pass
+
+    cov.stop()
+    cov.save()
+    print("\n\nCoverage Report:\n")
+    cov.report()
+    cov.html_report(directory='tmp/coverage')
+    cov.erase()
