ca64a2aa50cb29fc5fa49508a727309fa706c401
sepro <sebastian.proost@gmail.com>
2016-12-06 13:48:36 +0100

    cleaning up javascript code with linter

--- planet/static/js/planet_svg_legend.js
+++ planet/static/js/planet_svg_legend.js
@@ -1,137 +1,145 @@
+/*global $, document, Pablo */
 var svg_legend;
 var svg_families;
 var svg_labels;
 
 // constants for drawing grid legend
-var MARGIN_TOP = 150
-var MARGIN_LEFT = 85
-var HSPACE = 140
-var VSPACE = 80
-var COLS = 5
-var MAX_LABELS = 3
+var MARGIN_TOP = 150;
+var MARGIN_LEFT = 85;
+var HSPACE = 140;
+var VSPACE = 80;
+var COLS = 5;
+var MAX_LABELS = 3;
 
-$(function() {
-     var svg_file  = $('#legend').attr( "url" );
-     svg_legend = Pablo.load(svg_file, function(){
-        this.appendTo($('#legend'));
-        svg_legend.find('[edge_color]').attr('style', 'opacity:0');
-        svg_legend.find('[edge_color="color"]').attr('style', 'opacity:100')
+$(function () {
+    'use strict';
+    var svg_file  = $('#legend').attr('url'),
+        svg_legend = Pablo.load(svg_file, function () {
+            this.appendTo($('#legend'));
+            svg_legend.find('[edge_color]').attr('style', 'opacity:0');
+            svg_legend.find('[edge_color="color"]').attr('style', 'opacity:100');
 
-        svg_legend.find('[edge_width]').attr('style', 'opacity:0');
-        svg_legend.find('[edge_width="default_width"]').attr('style', 'opacity:100')
-
-        svg_legend.find('[node_color]').attr('style', 'opacity:0');
-        svg_legend.find('[node_color="color"]').attr('style', 'opacity:100')
-    });
-
+            svg_legend.find('[edge_width]').attr('style', 'opacity:0');
+            svg_legend.find('[edge_width="default_width"]').attr('style', 'opacity:100');
+            svg_legend.find('[node_color]').attr('style', 'opacity:0');
+            svg_legend.find('[node_color="color"]').attr('style', 'opacity:100');
+        });
 });
 
-$('.cy-edge-color').click(function(ev) {
+$('.cy-edge-color').click(function (ev) {
+    'use strict';
     ev.preventDefault();
     svg_legend.find('[edge_color]').attr('style', 'opacity:0');
-    svg_legend.find('[edge_color="' + $( this ).attr( 'attr' ) + '"]').attr('style', 'opacity:100');
-})
+    svg_legend.find('[edge_color="' + $(this).attr('attr') + '"]').attr('style', 'opacity:100');
+});
 
-$('.cy-edge-width').click(function(ev) {
+$('.cy-edge-width').click(function (ev) {
+    'use strict';
     ev.preventDefault();
     svg_legend.find('[edge_width]').attr('style', 'opacity:0');
-    svg_legend.find('[edge_width="' + $( this ).attr( 'attr' ) + '"]').attr('style', 'opacity:100');
-})
+    svg_legend.find('[edge_width="' + $(this).attr('attr') + '"]').attr('style', 'opacity:100');
+});
 
-$('.cy-node-color').click(function(ev) {
+$('.cy-node-color').click(function (ev) {
+    'use strict';
     ev.preventDefault();
     svg_legend.find('[node_color]').attr('style', 'opacity:0');
-    svg_legend.find('[node_color="' + $( this ).attr( 'attr' ) + '"]').attr('style', 'opacity:100');
-})
+    svg_legend.find('[node_color="' + $(this).attr('attr') + '"]').attr('style', 'opacity:100');
+});
 
-$('#cy-download-svg-legend').click(function(ev) {
-  ev.preventDefault();
+$('#cy-download-svg-legend').click(function (ev) {
+    'use strict';
+    ev.preventDefault();
 
-  var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(svg_legend));
-  element.setAttribute('download', "cytoscape_legend.svg");
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(svg_legend));
+    element.setAttribute('download', 'cytoscape_legend.svg');
 
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
-})
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+});
 
-$('#cy-download-png-legend').click(function(ev) {
-  ev.preventDefault();
+$('#cy-download-png-legend').click(function (ev) {
+    'use strict';
+    ev.preventDefault();
 
-  svg_legend.dataUrl('png', function(dataUrl){
-      var element = document.createElement('a');
-      element.setAttribute('href', dataUrl);
-      element.setAttribute('download', "cytoscape_legend.png");
-      element.setAttribute('style', 'display:none');
+    svg_legend.dataUrl('png', function (dataUrl) {
+        var element = document.createElement('a');
+        element.setAttribute('href', dataUrl);
+        element.setAttribute('download', "cytoscape_legend.png");
+        element.setAttribute('style', 'display:none');
 
-      document.body.appendChild(element);
-      element.click();
-      document.body.removeChild(element);
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
     });
-})
+});
 
 function generate_legend(label_set, attribute) {
-    var families = svg_legend.g().transform('translate', MARGIN_LEFT, MARGIN_TOP);
+    'use strict';
+    var families = svg_legend.g().transform('translate', MARGIN_LEFT, MARGIN_TOP),
+        col = 0,
+        row = 0,
+        color = null,
+        shape = null;
+
     families.attr('node_color', attribute);
     families.attr('style', 'opacity:0');
 
-    var col = 0;
-    var row = 0;
+    for (color in label_set) {
+        for (shape in label_set[color]) {
+            var label = (label_set[color][shape] === null) ? 'None' : label_set[color][shape],
+                new_label = families.g();
 
-    for (var color in label_set) {
-        for(var shape in label_set[color])
-        {
-            var label = (label_set[color][shape] === null) ? 'None' : label_set[color][shape];
-
-            var new_label = families.g();
-            new_label.transform('translate', col*HSPACE, row*VSPACE+15);
+            new_label.transform('translate', col * HSPACE, row * VSPACE + 15);
 
             if (shape === 'ellipse') {
-                new_label.circle({r:15, fill:color});
+                new_label.circle({r: 15, fill: color});
             } else if (shape === 'rectangle') {
-                new_label.rect({x:-15, y:-15, width:30, height:30, fill:color});
+                new_label.rect({x: -15, y: -15, width: 30, height: 30, fill: color});
             } else if (shape === 'roundrectangle') {
-                new_label.rect({x:-15, y:-15, rx:5, ry:5, width:30, height:30, fill:color});
+                new_label.rect({x: -15, y: -15, rx: 5, ry: 5, width: 30, height: 30, fill: color});
             } else if (shape === 'triangle') {
-                new_label.polygon({points:'0,-20 17,10 -17,10', fill:color});
+                new_label.polygon({points: '0,-20 17,10 -17,10', fill: color});
             } else if (shape === 'hexagon') {
-                new_label.polygon({points:'0,15 13,7.5 13,-7.5 0,-15 -13,-7.5 -13,7.5', fill:color})
+                new_label.polygon({points: '0,15 13,7.5 13,-7.5 0,-15 -13,-7.5 -13,7.5', fill: color})
                     .transform('rotate', 30);
             } else if (shape === 'octagon') {
-                new_label.polygon({points:'0,15 10.6,10.6 15,0 10.6,-10.6 0,-15 -10.6,-10.6 -15,0 -10.6,10.6', fill:color})
+                new_label.polygon({points: '0,15 10.6,10.6 15,0 10.6,-10.6 0,-15 -10.6,-10.6 -15,0 -10.6,10.6', fill: color})
                     .transform('rotate', 22.5);
             } else if (shape === 'vee') {
-                new_label.polygon({points:'0,20 17,-10 0,0 -17,-10', fill:color});
+                new_label.polygon({points: '0,20 17,-10 0,0 -17,-10', fill: color});
             } else if (shape === 'diamond') {
-                new_label.rect({x:-13, y:-13, width:26, height:26, fill:color})
-                .transform('rotate', 45);
+                new_label.rect({x: -13, y: -13, width: 26, height: 26, fill: color})
+                    .transform('rotate', 45);
             } else if (shape === 'rhomboid') {
-                new_label.rect({x:-13, y:-15, width:26, height:30, fill:color})
-                .transform('skewX', 30);
+                new_label.rect({x: -13, y: -15, width: 26, height: 30, fill: color})
+                    .transform('skewX', 30);
             }
 
-            label_group = new_label.g();
-            labels = label.split(';');
+            var label_group = new_label.g(),
+                labels = label.split(';');
             if (labels.length > MAX_LABELS) {
-                labels = labels.slice(0,MAX_LABELS);
+                labels = labels.slice(0, MAX_LABELS);
                 labels.push('...');
             }
 
-            for(var i = 0; i < labels.length; i++) {
-                var y = 15*i;
-                label_group.text({y:y, fill:"none", stroke:'#fff'}).content(labels[i])
+            var i = 0;
+            for (i = 0; i < labels.length; i += 1) {
+                var y = 15 * i;
+                label_group.text({y: y, fill: 'none', stroke: '#fff'}).content(labels[i])
                     .attr('text-anchor', 'middle')
                     .attr('font-family', 'sans-serif');
-                label_group.text({y:y, fill:"black"}).content(labels[i])
+                label_group.text({y: y, fill: 'black'}).content(labels[i])
                     .attr('text-anchor', 'middle')
                     .attr('font-family', 'sans-serif');
-                label_group.attr('height', y+15)
+                label_group.attr('height', y + 15);
             }
 
-            label_group.transform('translate', 0, -(label_group.attr('height')/2)+12);
+            label_group.transform('translate', 0, -(label_group.attr('height') / 2) + 12);
 
-            if (col < COLS-1) {
+            if (col < COLS - 1) {
                 col += 1;
             } else {
                 col = 0;
@@ -140,10 +148,11 @@ function generate_legend(label_set, attribute) {
         }
     }
 
-    var total_height = parseInt(((row+1)*VSPACE + MARGIN_TOP));
-    var document_height = parseInt(svg_legend.attr('height').replace('px',''));
+    var total_height = parseInt(((row + 1) * VSPACE + MARGIN_TOP), 10),
+        document_height = parseInt(svg_legend.attr('height').replace('px', ''), 10);
+        //viewbox = svg_legend.attr('viewBox');
+
     if (total_height > document_height) {
-        var viewbox = svg_legend.attr('viewBox');
         svg_legend.attr('viewBox', '0 0 750 ' + total_height);
         svg_legend.attr('height', total_height + 'px');
     }
--- planet/static/js/planet_xgmml.js
+++ planet/static/js/planet_xgmml.js
@@ -1,43 +1,47 @@
+/*global $, XMLWriter */
 /*
 Function to write XGMML file from cytoscape json object
 */
 
 function __componentToHex(c) {
+    'use strict';
     var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
+    return hex.length === 1 ? "0" + hex : hex;
 }
 
 function __rgbToHex(r, g, b) {
+    'use strict';
     return "#" + __componentToHex(r) + __componentToHex(g) + __componentToHex(b);
 }
 
-function __convertColor( color ) {
+function __convertColor(color) {
+    'use strict';
     if (color.indexOf('rgb(') > -1) {
-        values = color.replace('rgb','').replace('(','').replace(')','');
-        rgb = values.split(", ");
-        return __rgbToHex(parseInt(rgb[0]), parseInt(rgb[1]), parseInt(rgb[2])).toLowerCase();
+        var values = color.replace('rgb', '').replace('(', '').replace(')', ''),
+            rgb = values.split(", ");
+        return __rgbToHex(parseInt(rgb[0], 10), parseInt(rgb[1], 10), parseInt(rgb[2], 10)).toLowerCase();
     }
-    else if (color.length === 7) {
+    if (color.length === 7) {
         return color.toLowerCase();
-    } else {
-        c = '#' + color[1] + color[1] + color[2] + color[2] + color[3] + color[3];
-        return c.toLowerCase();
     }
-
+    var c = '#' + color[1] + color[1] + color[2] + color[2] + color[3] + color[3];
+    return c.toLowerCase();
 }
 
-function __convertShape ( shape ) {
+function __convertShape(shape) {
+    'use strict';
     if (shape === 'rhomboid') {
         return 'RHOMBUS';
-    } else if (shape === 'roundrectangle') {
-        return 'ROUND_RECTANGLE';
-    } else {
-        return shape.toUpperCase();
     }
+    if (shape === 'roundrectangle') {
+        return 'ROUND_RECTANGLE';
+    }
+    return shape.toUpperCase();
+
 }
 
 function writeXGMML(data) {
-
+    'use strict';
     var xw = new XMLWriter('UTF-8');
     xw.formatting = 'indented';//add indentation and newlines
     xw.indentChar = ' ';//indent with spaces
@@ -45,91 +49,91 @@ function writeXGMML(data) {
 
     xw.writeStartDocument( );
 
-    xw.writeStartElement( 'graph' );
-    xw.writeAttributeString( 'id', 'planet_cytoscape_network');
-    xw.writeAttributeString( 'label', 'planet_cytoscape_network');
-    xw.writeAttributeString( 'xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
-    xw.writeAttributeString( 'xmlns:dc', 'http://purl.org/dc/elements/1.1/');
-    xw.writeAttributeString( 'xmlns:rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
-    xw.writeAttributeString( 'xmlns', 'http://www.cs.rpi.edu/XGMML');
+    xw.writeStartElement('graph');
+    xw.writeAttributeString('id', 'planet_cytoscape_network');
+    xw.writeAttributeString('label', 'planet_cytoscape_network');
+    xw.writeAttributeString('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+    xw.writeAttributeString('xmlns:dc', 'http://purl.org/dc/elements/1.1/');
+    xw.writeAttributeString('xmlns:rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+    xw.writeAttributeString('xmlns', 'http://www.cs.rpi.edu/XGMML');
 
-        xw.writeStartElement( 'att' );
-        xw.writeAttributeString( 'name', 'documentVersion');
-        xw.writeAttributeString( 'value', '1.0');
-        xw.writeEndElement();
+    xw.writeStartElement('att');
+    xw.writeAttributeString('name', 'documentVersion');
+    xw.writeAttributeString('value', '1.0');
+    xw.writeEndElement();
 
-        xw.writeStartElement( 'att' );
-        xw.writeAttributeString( 'name', 'backgroundColor');
-        xw.writeAttributeString( 'value', '#ffffff');
-        xw.writeEndElement();
+    xw.writeStartElement('att');
+    xw.writeAttributeString('name', 'backgroundColor');
+    xw.writeAttributeString('value', '#ffffff');
+    xw.writeEndElement();
 
-        data.nodes().forEach( function(node) {
-              if (!node.data('compound'))
-              {
-                  xw.writeStartElement( 'node' );
-                  xw.writeAttributeString( 'id', node.data('id') );
-                  xw.writeAttributeString( 'label', node.data('name') );
-                  xw.writeAttributeString( 'name', 'base' );
+    data.nodes().forEach( function(node) {
+        if (!node.data('compound'))
+        {
+            xw.writeStartElement('node');
+            xw.writeAttributeString('id', node.data('id') );
+            xw.writeAttributeString('label', node.data('name') );
+            xw.writeAttributeString('name', 'base');
 
-                        xw.writeStartElement( 'att' );
-                        xw.writeAttributeString( 'label', 'gene_name' );
-                        xw.writeAttributeString( 'name', 'gene_name' );
-                        xw.writeAttributeString( 'value', node.data('gene_name') );
-                        xw.writeAttributeString( 'type', 'string');
-                        xw.writeEndElement();
+                  xw.writeStartElement('att');
+                  xw.writeAttributeString('label', 'gene_name');
+                  xw.writeAttributeString('name', 'gene_name');
+                  xw.writeAttributeString('value', node.data('gene_name'));
+                  xw.writeAttributeString('type', 'string');
+                  xw.writeEndElement();
 
-                        xw.writeStartElement( 'graphics' );
-                        xw.writeAttributeString( 'width', '1' );
-                        xw.writeAttributeString( 'fill', __convertColor(node.renderedStyle('background-color')) );
-                        xw.writeAttributeString( 'outline', "#000000" );
-                        xw.writeAttributeString( 'x', node.position('x'));
-                        xw.writeAttributeString( 'y', node.position('y'));
-                        xw.writeAttributeString( 'h', '30.0');
-                        xw.writeAttributeString( 'w', '30.0');
-                        xw.writeAttributeString( 'type', __convertShape(node.renderedStyle('shape')) );
+                  xw.writeStartElement('graphics');
+                  xw.writeAttributeString('width', '1' );
+                  xw.writeAttributeString('fill', __convertColor(node.renderedStyle('background-color')));
+                  xw.writeAttributeString('outline', "#000000" );
+                  xw.writeAttributeString('x', node.position('x'));
+                  xw.writeAttributeString('y', node.position('y'));
+                  xw.writeAttributeString('h', '30.0');
+                  xw.writeAttributeString('w', '30.0');
+                  xw.writeAttributeString('type', __convertShape(node.renderedStyle('shape')));
 
-                            xw.writeStartElement( 'att' );
-                            xw.writeAttributeString( 'name', 'cytoscapeNodeGraphicsAttributes' );
-                                xw.writeStartElement( 'att' );
-                                xw.writeAttributeString( 'name', 'nodeTransparency' );
-                                xw.writeAttributeString( 'value', '1.0' );
-                                xw.writeEndElement();
+                      xw.writeStartElement('att');
+                      xw.writeAttributeString('name', 'cytoscapeNodeGraphicsAttributes');
+                          xw.writeStartElement('att');
+                          xw.writeAttributeString('name', 'nodeTransparency');
+                          xw.writeAttributeString('value', '1.0');
+                          xw.writeEndElement();
 
-                                xw.writeStartElement( 'att' );
-                                xw.writeAttributeString( 'name', 'nodeLabelFont' );
-                                xw.writeAttributeString( 'value', 'Default-0-12' );
-                                xw.writeEndElement();
+                          xw.writeStartElement('att');
+                          xw.writeAttributeString('name', 'nodeLabelFont');
+                          xw.writeAttributeString('value', 'Default-0-12');
+                          xw.writeEndElement();
 
-                                xw.writeStartElement( 'att' );
-                                xw.writeAttributeString( 'name', 'borderLineType' );
-                                xw.writeAttributeString( 'value', 'solid' );
-                                xw.writeEndElement();
-                            xw.writeEndElement();
-
-                        xw.writeEndElement();
+                          xw.writeStartElement('att');
+                          xw.writeAttributeString('name', 'borderLineType');
+                          xw.writeAttributeString('value', 'solid');
+                          xw.writeEndElement();
+                      xw.writeEndElement();
 
                   xw.writeEndElement();
-              }
-        });
-
-        data.edges().forEach( function(edge) {
-            xw.writeStartElement( 'edge' );
-            xw.writeAttributeString( 'id', edge.data('id') );
-            xw.writeAttributeString( 'label', edge.data('id') );
-            xw.writeAttributeString( 'source', edge.data('source') );
-            xw.writeAttributeString( 'target', edge.data('target') );
-
-                xw.writeStartElement( 'graphics' );
-                xw.writeAttributeString( 'width', edge.renderedStyle('width').replace('px', '') );
-                xw.writeAttributeString( 'fill', __convertColor(edge.renderedStyle('line-color')) );
-                if (edge.data('homology')) {
-                    xw.writeAttributeString( 'EDGE_LINE_TYPE', 'LONG_DASH' );
-                }
-                xw.writeEndElement();
 
             xw.writeEndElement();
+        }
+    });
 
-        });
+    data.edges().forEach( function(edge) {
+        xw.writeStartElement('edge');
+        xw.writeAttributeString('id', edge.data('id'));
+        xw.writeAttributeString('label', edge.data('id'));
+        xw.writeAttributeString('source', edge.data('source'));
+        xw.writeAttributeString('target', edge.data('target'));
+
+            xw.writeStartElement('graphics');
+            xw.writeAttributeString('width', edge.renderedStyle('width').replace('px', ''));
+            xw.writeAttributeString('fill', __convertColor(edge.renderedStyle('line-color')));
+            if (edge.data('homology')) {
+                xw.writeAttributeString('EDGE_LINE_TYPE', 'LONG_DASH');
+            }
+            xw.writeEndElement();
+
+        xw.writeEndElement();
+
+    });
 
     xw.writeEndElement();
     xw.writeEndDocument();
