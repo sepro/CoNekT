b1aa4364bb9a361d49aa0eac5a4b9145d0a74bc1
sepro <sebastian.proost@gmail.com>
2016-12-14 11:53:55 +0100

    removed debug things

--- planet/models/blast_db.py
+++ planet/models/blast_db.py
@@ -18,15 +18,11 @@ class BlastDB:
         CDS_FASTA_PATH = os.path.join(TEMP_DIR, 'cds.fasta')
         PEP_FASTA_PATH = os.path.join(TEMP_DIR, 'pep.fasta')
 
-        print(TEMP_DIR, CDS_FASTA_PATH, PEP_FASTA_PATH)
-
         Sequence.export_cds(CDS_FASTA_PATH)
         Sequence.export_protein(PEP_FASTA_PATH)
 
         build_cds_cmd = current_app.config['MAKEBLASTDB_NUCL_CMD'].replace('<IN>', '"' + CDS_FASTA_PATH + '"')
         build_pep_cmd = current_app.config['MAKEBLASTDB_PROT_CMD'].replace('<IN>', '"' + PEP_FASTA_PATH + '"')
-
-        print(build_cds_cmd, build_pep_cmd)
 
         subprocess.call(shlex.split(build_cds_cmd))
         subprocess.call(shlex.split(build_pep_cmd))
