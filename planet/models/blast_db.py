6435aa4fa2e9a274984b16761571a971ac21fae5
sepro <sebastian.proost@gmail.com>
2016-12-14 11:36:25 +0100

    debugging

--- planet/models/blast_db.py
+++ planet/models/blast_db.py
@@ -26,6 +26,8 @@ class BlastDB:
         build_cds_cmd = current_app.config['MAKEBLASTDB_NUCL_CMD'].replace('<IN>', '"' + CDS_FASTA_PATH + '"')
         build_pep_cmd = current_app.config['MAKEBLASTDB_PROT_CMD'].replace('<IN>', '"' + PEP_FASTA_PATH + '"')
 
+        print(build_cds_cmd, build_pep_cmd)
+
         subprocess.call(shlex.split(build_cds_cmd))
         subprocess.call(shlex.split(build_pep_cmd))
 
