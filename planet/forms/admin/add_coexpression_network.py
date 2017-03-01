40105d6224e7450b1618a2ab6ddbbc4e126edc17
sepro <sebastian.proost@gmail.com>
2017-03-01 09:12:30 +0100

    allow pure HRR network (no filter on PCC)

--- planet/forms/admin/add_coexpression_network.py
+++ planet/forms/admin/add_coexpression_network.py
@@ -13,7 +13,7 @@ class AddCoexpressionNetworkForm(FlaskForm):
 
     limit = SelectField('Limit', coerce=int, choices=[(i, i) for i in range(10, 100, 10)], default=30)
     pcc_cutoff = SelectField('PCC-cutoff', coerce=float,
-                             choices=[(i, i) for i in [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4]], default=0.7)
+                             choices=[(i, i) for i in [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, -2.0]], default=-2.0)
 
     file = FileField()
 
