4f21b0004d39e30ad6707052b018007347110286
sepro <sebastian.proost@gmail.com>
2017-03-16 16:28:14 +0100

    got inchlib (interactive heatmap) working with clusters. Still rought draft

--- planet/controllers/heatmap.py
+++ planet/controllers/heatmap.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, Response
+
+import json
 
 from planet import cache
 from planet.forms.heatmap import HeatmapForm
@@ -64,3 +66,33 @@ def heatmap_main():
                                form=form)
     else:
         return render_template("expression_heatmap.html", form=form)
+
+
+@heatmap.route('/inchlib/j/<cluster_id>.json')
+@cache.cached()
+def heatmap_inchlib_json(cluster_id):
+    cluster = CoexpressionCluster.query.get(cluster_id)
+    associations = SequenceCoexpressionClusterAssociation.query.filter_by(coexpression_cluster_id=cluster_id).all()
+
+    probes = [a.probe for a in associations]
+
+    current_heatmap = ExpressionProfile.get_heatmap(cluster.method.network_method.species_id, probes)
+
+    output = {"data": {
+            "nodes": {a["name"]: {
+                "count": 1,
+                "distance": 0,
+                "features": [a["values"][o] for o in current_heatmap["order"]],
+                "objects": [a["name"]]
+            } for a in current_heatmap['heatmap_data']},
+            "feature_names": [o for o in current_heatmap["order"]]
+        }
+    }
+
+    return Response(json.dumps(output, sort_keys=True, indent=4, separators=(',', ': ')), mimetype='application/json')
+
+
+@heatmap.route('/inchlib/<cluster_id>')
+@cache.cached()
+def heatmap_inchlib(cluster_id):
+    return render_template("inchlib_heatmap.html", cluster_id=cluster_id)
