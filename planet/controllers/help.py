655f0fa393e239147fe3cac773bf04b1578709d6
sepro <sebastian.proost@gmail.com>
2017-09-19 13:53:40 +0200

    added help and popup

--- planet/controllers/help.py
+++ planet/controllers/help.py
@@ -14,7 +14,10 @@ __TOPICS = {"gene_family": "modals/help/gene_family.html",
             "neighborhood": "modals/help/neighborhood.html",
             "hrr": "modals/help/hrr.html",
             "blast": "modals/help/blast.html",
-            "pcc": "modals/help/pcc.html"}
+            "pcc": "modals/help/pcc.html",
+            "lowabundance": "modals/help/low_abundance.html"}
+
+__POPUPS = {"lowabundance": "tooltips/low_abundance.html"}
 
 help = Blueprint('help', __name__)
 
@@ -29,6 +32,20 @@ def help_topic(topic):
     """
     if topic in __TOPICS.keys():
         return render_template(__TOPICS[topic])
+    else:
+        abort(404)
+
+
+@help.route('/popup/<topic>')
+def help_popup(topic):
+    """
+    Generic function to get help popups
+
+    :param topic: name of the topic
+    :return: renders template for the topic if found, 404 if not.
+    """
+    if topic in __POPUPS.keys():
+        return render_template(__POPUPS[topic])
     else:
         abort(404)
 
