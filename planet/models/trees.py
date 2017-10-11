09089ba2db7e797db9eabaa183767cf209b5696c
sepro <sebastian.proost@gmail.com>
2017-10-11 15:55:33 +0200

    updated phyd3 (contained bug)

--- planet/models/trees.py
+++ planet/models/trees.py
@@ -136,9 +136,12 @@ class Tree(db.Model):
         return tree.ascii_art()
 
     @staticmethod
-    def __yattag_node(node, tag, text, line, id_to_clade, seq_to_species, seq_to_id):
+    def __yattag_node(node, tag, text, line, id_to_clade, seq_to_species, seq_to_id, root=1):
         with tag('clade'):
-            line('branch_length', node.length)
+            if root == 1:
+                line('branch_length', 0.1)
+            else:
+                line('branch_length', node.length)
             if node.is_leaf:
                     line('name', node.name)
                     if node.name in seq_to_id.keys():
@@ -167,7 +170,7 @@ class Tree(db.Model):
                         line('speciations', 1)
 
                 for d in node.descendants:
-                    Tree.__yattag_node(d, tag, text, line, id_to_clade, seq_to_species, seq_to_id)
+                    Tree.__yattag_node(d, tag, text, line, id_to_clade, seq_to_species, seq_to_id, root=0)
 
     @property
     def phyloxml(self):
@@ -211,6 +214,27 @@ class Tree(db.Model):
                 # line('description', "PlaNet 2.0 PhyloXML tree")
                 Tree.__yattag_node(tree, tag, text, line, id_to_clade, seq_to_species, seq_to_id)
 
+            if has_heatmap:
+                with tag('graphs'):
+                    with tag('graph', type="heatmap"):
+                        line('name', 'heatmap')
+                        with tag('legend', show=1):
+                            for label in heatmap_order:
+                                with tag('field'):
+                                    line('name', label)
+                            with tag('gradient'):
+                                line('name', 'YlGnBu')
+                                line('classes', len(heatmap_order))
+                        with tag('data'):
+                            for cd in csep_data:
+                                if "profile" in cd.keys() and "data" in cd["profile"].keys():
+                                    with tag('values', **{'for': str(cd["sequence_id"])}):
+                                        for label in heatmap_order:
+                                            if cd["profile"]["data"][label] is not None:
+                                                line('value', cd["profile"]["data"][label])
+                                            else:
+                                                line('value', '')
+
             with tag('taxonomies'):
                 for s in species:
                     with tag('taxonomy', code=s.code):
@@ -223,27 +247,6 @@ class Tree(db.Model):
                         line('color', '0x000000')
                         line('name', c.name)
                         line('url', url_for('clade.clade_view', clade_id=c.id, _external=True))
-
-            if has_heatmap:
-                with tag('graphs'):
-                    with tag('graph', type="heatmap"):
-                        line('name', 'heatmap')
-                        with tag('legend', show=1):
-                            for label in heatmap_order:
-                                with tag('field'):
-                                    line('name', label)
-                            with tag('gradient'):
-                                line('name', 'YlOrRd')
-                                line('classes', len(heatmap_order))
-                        with tag('data'):
-                            for cd in csep_data:
-                                if "profile" in cd.keys() and "data" in cd["profile"].keys():
-                                    with tag('values', **{'for': str(cd["sequence_id"])}):
-                                        for label in heatmap_order:
-                                            if cd["profile"]["data"][label] is not None:
-                                                line('value', cd["profile"]["data"][label])
-                                            else:
-                                                line('value', '')
 
         return indent(doc.getvalue())
 
