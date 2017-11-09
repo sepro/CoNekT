8c35158f05dbbaac757f8b8c51d9a4fb248f4dd7
sepro <sebastian.proost@gmail.com>
2017-11-09 17:21:23 +0100

    hiding trees when they haven not been reconciled yet. updated names in plot

--- conekt/controllers/admin/views/xrefs.py
+++ conekt/controllers/admin/views/xrefs.py
@@ -1,4 +1,5 @@
 from flask_admin import expose
+from flask import current_app, flash
 
 from conekt.controllers.admin.views import AdminBaseView
 from conekt.forms.admin.add_xrefs import AddXRefsForm, AddXRefsFamiliesForm
@@ -13,6 +14,9 @@ class AddXRefsView(AdminBaseView):
         form = AddXRefsForm()
         form.populate_species()
 
+        if current_app.config['WHOOSHEE_ENABLE_INDEXING']:
+            flash("WHOOSHEE Indexing is enabled, this can take a long time to complete.", "warning")
+
         return self.render('admin/add/xrefs.html', form=form)
 
 
@@ -24,5 +28,8 @@ class AddXRefsFamiliesView(AdminBaseView):
     def index(self):
         form = AddXRefsFamiliesForm()
         form.populate_methods()
+
+        if current_app.config['WHOOSHEE_ENABLE_INDEXING']:
+            flash("WHOOSHEE Indexing is enabled, this can take a long time to complete.", "warning")
 
         return self.render('admin/add/xrefs_families.html', form=form)
