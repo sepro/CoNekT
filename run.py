d554c495fc01890933b653a5ac54d29e596ced9e
sepro <sebastian.proost@gmail.com>
2018-07-19 12:58:20 +0200

    added check if folder for sqlitedb exists. If not it will create the folder.

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import click
+import os
 from conekt import create_app, db
 from conekt.models.users import User
 
@@ -10,6 +11,13 @@ app = create_app('config')
 def initdb():
     """Initialize the database."""
     click.echo('Init the db')
+    SQLALCHEMY_DATABASE_URI = app.config['SQLALCHEMY_DATABASE_URI']
+
+    if SQLALCHEMY_DATABASE_URI.startswith('sqlite:///'):
+        path = os.path.dirname(os.path.realpath(SQLALCHEMY_DATABASE_URI.replace('sqlite:///', '')))
+        if not os.path.exists(path):
+            os.makedirs(path)
+
     db.create_all(app=app)
 
     if len(User.query.all()) == 0:
